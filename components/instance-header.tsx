cd64ff859c9a0a1efcb252f18433cb74aaf3042f
Deepkumar chadamiya <53164542+deep8904@users.noreply.github.com>
2026-06-18 15:39:23 -0700

    Refresh Loose Thread LT mark

--- components/instance-header.tsx
+++ components/instance-header.tsx
@@ -35,6 +35,10 @@ export function InstanceHeader({ writingCount }: InstanceHeaderProps) {
     <>
       <header className="site-header">
         <Link className="wordmark" href="/" aria-label={`${siteConfig.name} home`}>
+          <span className="wordmark-mark" aria-hidden="true">
+            <span>L</span><span>T</span>
+            <i />
+          </span>
           <span>LOOSE</span><b aria-hidden="true">/</b><span>THREAD</span>
         </Link>
 
